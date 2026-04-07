581776cd697d8dad46300b99106a9b9431156413
MatiSama1995 <43218629+MatiSama1995@users.noreply.github.com>
2026-04-07 09:45:37 -0400

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA9jvefDipf0JrNNm6Pm6EPe5EuuM3Mqdo",
+    authDomain: "gestion-certificaciones.firebaseapp.com",
+    projectId: "gestion-certificaciones",
+    storageBucket: "gestion-certificaciones.firebasestorage.app",
+    messagingSenderId: "182274949963",
+    appId: "1:182274949963:web:20486d47687f6a62729ece"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const firestoreAppId = "certitrack-v1";
